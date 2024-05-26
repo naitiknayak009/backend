7f591e0dece808a117ac6629c9616b2ca31e746e
Naitik Nayak <naitiknayak009@gmail.com>
2024-05-26 11:34:31 +0530

    connection db

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () =>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`MongoDB connected !! ${mongoose.connection.host}`);
+    } catch (error) {
+        console.log("error" , error);
+        process.exit(1)
+    }
+}
+export default connectDB;
